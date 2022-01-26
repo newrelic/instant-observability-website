f11b35ab1bffb292c9e5ec623de89dc7bcb653fc
aswanson-nr <aswanson@newrelic.com>
2022-01-25 16:31:51 -0800

    feat: remove unused gatsby plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -146,7 +146,7 @@ module.exports = {
         typeName: 'Quickstarts',
       },
     },
-    'gatsby-plugin-mdx',
+    'gatsby-plugin-mdx', // Not used, but required by the theme
     'gatsby-plugin-meta-redirect',
     'gatsby-plugin-use-query-params',
     {
