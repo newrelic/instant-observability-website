03adee7619b840e1c90f177e3f693abf6fbd0b9a
Kris Kenney <70179215+nr-kkenney@users.noreply.github.com>
2022-03-07 14:27:21 -0800

    chore: updates to customNewRelicEvent
    * the account we are targetting is not a production account, so swapping
    to staging url.
    * added environment variable to help distinguish events between
    environments -- local development, PRs, and production.
    * the key we were using before wasn't correct. Also, the header for the
    key we want to use is incorrect. Before I add a new key outright, want
    to test everything works.
    * also added a comment indicating this module is only intended for use
    with SSR pages.

--- src/utils/customNewRelicEvent.js
+++ src/utils/customNewRelicEvent.js
@@ -1,7 +1,12 @@
+/**
+ * This module is intended for use only with SSR pages.
+ * For monitoring & creating events for browser pages, use the browser agent instead.
+ */
 
 // taken from gatsby-config.js
 const NEW_RELIC_ACCOUNT_ID = '10956800';
-const NEW_RELIC_LICENSE_KEY = 'NRJS-649173eb1a7b28cd6ab';
+const NEW_RELIC_LICENSE_KEY = process.env.NEW_RELIC_LICENSE_KEY;
+const ENVIRONMENT = process.env.BRANCH || 'local';
 
 /**
  * Helper function to make an API request to the Events API.
@@ -12,7 +17,12 @@ const NEW_RELIC_LICENSE_KEY = 'NRJS-649173eb1a7b28cd6ab';
  * @returns {Promise<boolean>} Whether or not the request was (eventually) successful
  */
 const apiRequest = async (key, accountId, data) => {
-  const url = `https://insights-collector.newrelic.com/v1/accounts/${accountId}/events`;
+  const url = `https://staging-insights-collector.newrelic.com/v1/accounts/${accountId}/events`;
+
+  if (!NEW_RELIC_LICENSE_KEY) {
+    console.error('NEW_RELIC_LICENSE_KEY is not set');
+    return false;
+  }
 
   try {
     await fetch(url, {
@@ -44,6 +54,7 @@ const track = async (eventType, metadata = {}) =>
   apiRequest(NEW_RELIC_LICENSE_KEY, NEW_RELIC_ACCOUNT_ID, {
     eventType,
     account: NEW_RELIC_ACCOUNT_ID,
+    environment: ENVIRONMENT,
     ...metadata,
   });
 
