c06359b24731990d6cecf71ecd5ededcc1bdff59
aswanson-nr <aswanson@newrelic.com>
2022-03-08 15:37:18 -0800

    feat: refine mobile category selection
    * change mobile category selection to use general category picker
      component

--- src/components/CategorySelector.js
+++ src/components/CategorySelector.js
@@ -10,38 +10,19 @@ const CategorySelector = ({
   totalQuickstartCount,
 }) => (
   <FormControl>
-    <Label htmlFor="quickstartCategory">CATEGORIES</Label>
-    <Button
-      type="button"
-      key="all"
-      disabled={totalQuickstartCount === 0}
-      onClick={() => handleCategory([])}
+    <Label htmlFor="quickstartCategory">Categories</Label>
+    <div
       css={css`
-        padding: 1rem 0.5rem;
+        overflow-y: scroll;
+        padding-bottom: 3rem;
         width: 100%;
-        display: flex;
-        justify-content: flex-start;
-        color: var(--primary-text-color);
-        font-weight: 100;
-        background: ${category === [].toString()
-          ? 'var(--divider-color)'
-          : 'none'};
       `}
     >
-      All
-      <span
-        css={css`
-          color: var(--secondary-text-color);
-          padding-left: 0.25rem;
-        `}
-      >{`(${totalQuickstartCount})`}</span>
-    </Button>
-    {categoriesWithCount.map(({ displayName, terms, slug, count }) => (
       <Button
         type="button"
-        key={slug}
-        disabled={count === 0}
-        onClick={() => handleCategory(terms)}
+        key="all"
+        disabled={totalQuickstartCount === 0}
+        onClick={() => handleCategory([])}
         css={css`
           padding: 1rem 0.5rem;
           width: 100%;
@@ -49,20 +30,48 @@ const CategorySelector = ({
           justify-content: flex-start;
           color: var(--primary-text-color);
           font-weight: 100;
-          background: ${category === terms.toString()
+          background: ${category === [].toString()
             ? 'var(--divider-color)'
             : 'none'};
         `}
       >
-        {`${displayName}`}
+        All
         <span
           css={css`
             color: var(--secondary-text-color);
             padding-left: 0.25rem;
           `}
-        >{`(${count})`}</span>
+        >{`(${totalQuickstartCount})`}</span>
       </Button>
-    ))}
+
+      {categoriesWithCount.map(({ displayName, terms, slug, count }) => (
+        <Button
+          type="button"
+          key={slug}
+          disabled={count === 0}
+          onClick={() => handleCategory(terms)}
+          css={css`
+            padding: 1rem 0.5rem;
+            width: 100%;
+            display: flex;
+            justify-content: flex-start;
+            color: var(--primary-text-color);
+            font-weight: 100;
+            background: ${category === terms.toString()
+              ? 'var(--divider-color)'
+              : 'none'};
+          `}
+        >
+          {`${displayName}`}
+          <span
+            css={css`
+              color: var(--secondary-text-color);
+              padding-left: 0.25rem;
+            `}
+          >{`(${count})`}</span>
+        </Button>
+      ))}
+    </div>
   </FormControl>
 );
 
