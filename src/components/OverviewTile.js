1a38c656bcf2f55499a6ef31407cc87aebb29767
RavitejaSurampudi <ravitejasurampudi@newrelic.com>
2022-03-10 09:35:12 +0530

    chore: CXPUI-22 chore tiles in overview as clickable

--- src/components/OverviewTile.js
+++ src/components/OverviewTile.js
@@ -2,7 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { css } from '@emotion/react';
 
-import { Surface, Tag } from '@newrelic/gatsby-theme-newrelic';
+import { Link, Surface, Tag } from '@newrelic/gatsby-theme-newrelic';
+import useTabs from './Tabs/useTabs';
 
 const truncateDescription = (description) => {
   if (description.length > 250) {
@@ -13,12 +14,33 @@ const truncateDescription = (description) => {
 };
 
 const OverviewTile = ({ key, title, image, description, tag }) => {
+  const [currentTab, setCurrentTab] = useTabs();
+
+  const navigateToTab = (tag) => {
+    switch (tag) {
+      case 'Dashboard':
+        setCurrentTab('dashboards');
+        break;
+      case 'Alert':
+        setCurrentTab('alerts');
+        break;
+      case 'Doc':
+        setCurrentTab('data-sources');
+        break;
+      default:
+        setCurrentTab('overview');
+        break;
+    }
+  };
+
   return (
     <Surface key={key} base={Surface.BASE.PRIMARY}>
       <div
         css={css`
           padding: 1em;
+          cursor: pointer;
         `}
+        onClick={() => navigateToTab(tag)}
       >
         <h3>{title}</h3>
         <div>
