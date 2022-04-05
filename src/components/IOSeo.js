37465c112a9c256ddfee80fac8370d3d02dc3029
Kris Kenney <70179215+nr-kkenney@users.noreply.github.com>
2022-04-05 10:37:24 -0700

    chore: remove extra tessen script
    * since we fixed issues around downloading & using tessen, we no longer need this extra script block (which also had a hardcoded old version of tessen)

--- src/components/IOSeo.js
+++ src/components/IOSeo.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { SEO as Seo } from '@newrelic/gatsby-theme-newrelic';
-import { useStaticQuery, graphql, withPrefix } from 'gatsby';
+import { useStaticQuery, graphql } from 'gatsby';
 
-function IOSeo({ description, meta, title, tags, location, type }) {
+function IOSeo ({ description, meta, title, tags, location, type }) {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -103,7 +103,6 @@ function IOSeo({ description, meta, title, tags, location, type }) {
       {validMetadata.map((data, index) => (
         <meta key={`${data.name}-${index}`} {...data} />
       ))}
-      <script src={withPrefix('tessen.min-1.3.0.js')} type="text/javascript" />
     </Seo>
   );
 }
