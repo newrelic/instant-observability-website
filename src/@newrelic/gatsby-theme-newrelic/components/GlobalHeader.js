35f7fc012c9ed8bba3b7c4ec77099aa3513dee5e
sjyothi54 <sjyothi@newrelic.com>
2022-04-08 20:44:25 +0530

    updated the changes

--- src/@newrelic/gatsby-theme-newrelic/components/GlobalHeader.js
+++ src/@newrelic/gatsby-theme-newrelic/components/GlobalHeader.js
@@ -10,7 +10,7 @@ import GlobalNavLink from '@newrelic/gatsby-theme-newrelic/src/components/Global
 import useMedia from 'use-media';
 import useThemeTranslation from '@newrelic/gatsby-theme-newrelic/src/hooks/useThemeTranslation';
 import { useInstrumentedHandler } from '@newrelic/gatsby-theme-newrelic';
-import { Menu, X } from "react-feather"
+import { Menu, X } from 'react-feather';
 
 const action = css`
   color: var(--secondary-text-color);
@@ -77,7 +77,7 @@ const createNavList = (listType, activeSite = null) => {
 const CONDENSED_BREAKPOINT = '815px';
 
 // swaps out logo into collapsable nav
-const NAV_BREAKPOINT = '770px';
+const NAV_BREAKPOINT = '1035px';
 
 // changes layout for mobile view
 const MOBILE_BREAKPOINT = '600px';
@@ -89,15 +89,15 @@ const actionIcon = css`
 
 const GlobalHeader = ({ className, activeSite }) => {
   const { t } = useThemeTranslation();
-  const [isOpen, setOpen] = React.useState(false)
+  const [isOpen, setOpen] = React.useState(false);
 
   React.useEffect(() => {
     if (isOpen) {
-      document.body.style.overflowY = "hidden"
+      document.body.style.overflowY = 'hidden';
     } else {
-      document.body.style.overflowY = "visible"
+      document.body.style.overflowY = 'visible';
     }
-  }, [isOpen])
+  }, [isOpen]);
 
   const hideLogoText = useMedia({ maxWidth: '350px' });
 
@@ -117,15 +117,15 @@ const GlobalHeader = ({ className, activeSite }) => {
             }
           `,
           css`
-          background-color: var(--color-white);
-          box-shadow: var(--shadow-2);
-          top: 0;
-          z-index: 80;
+            background-color: var(--color-white);
+            box-shadow: var(--shadow-2);
+            top: 0;
+            z-index: 80;
 
-          .dark-mode & {
-            background-color: var(--color-neutrals-800);
-          }
-          `
+            .dark-mode & {
+              background-color: var(--color-neutrals-800);
+            }
+          `,
         ]}
       >
         <div
@@ -140,37 +140,37 @@ const GlobalHeader = ({ className, activeSite }) => {
             align-items: center;
           `}
         >
-             <a
-              href="https://newrelic.com"
-              onClick={useInstrumentedHandler(null, {
-                eventName: 'externalLinkClick',
-                category: 'LinkClick',
-                origin: 'globalHeader',
-                href: 'https://newrelic.com',
-              })}
-              css={css`
-                display: flex;
-                align-items: center;
-                margin-right: 1rem;
+          <a
+            href="https://newrelic.com"
+            onClick={useInstrumentedHandler(null, {
+              eventName: 'externalLinkClick',
+              category: 'LinkClick',
+              origin: 'globalHeader',
+              href: 'https://newrelic.com',
+            })}
+            css={css`
+              display: flex;
+              align-items: center;
+              margin-right: 1rem;
 
-                @media screen and (max-width: ${NAV_BREAKPOINT}) {
-                  display: none;
+              @media screen and (max-width: ${NAV_BREAKPOINT}) {
+                display: none;
+              }
+            `}
+          >
+            <NewRelicLogo
+              size="176px"
+              css={css`
+                .logo-text {
+                  fill: var(--color-neutrals-900);
+
+                  .dark-mode & {
+                    fill: var(--color-neutrals-100);
+                  }
                 }
               `}
-            >
-              <NewRelicLogo
-                size="176px"
-                css={css`
-                  .logo-text {
-                    fill: var(--color-neutrals-900);
-
-                    .dark-mode & {
-                      fill: var(--color-neutrals-100);
-                    }
-                  }
-                `}
-              />
-            </a>
+            />
+          </a>
           <nav
             css={css`
               display: flex;
@@ -203,11 +203,10 @@ const GlobalHeader = ({ className, activeSite }) => {
               }
             `}
           >
-
             <ul
               css={css`
                 height: 100%;
-                display:flex;
+                display: flex;
                 justify-conten: center;
                 padding: 0;
                 display: flex;
@@ -217,19 +216,18 @@ const GlobalHeader = ({ className, activeSite }) => {
                 position: relative;
                 -webkit-overflow-scrolling: touch;
                 -ms-overflow-style: -ms-autohiding-scrollbar;
-                
                 > li {
                   margin: 1.3rem 0;
                   padding: 0px 10px;
                   flex: 0 0 auto;
                   > a {
-                   --active-color: var(--color-white);
-                   font-family: Open Sans,Open Sans,Corbel,Arial,sans-serif;
-                   color: #54565a;
-                   font-weight: 400;
-                   font-size: 1rem;
-                   padding: 17px 0px;
-                   display:block;
+                    --active-color: var(--color-white);
+                    font-family: Open Sans, Open Sans, Corbel, Arial, sans-serif;
+                    color: #54565a;
+                    font-weight: 400;
+                    font-size: 1rem;
+                    padding: 17px 0px;
+                    display: block;
                     &:hover {
                       color: #54565a;
                       border-bottom: 3px solid var(--color-brand-400);
@@ -264,8 +262,8 @@ const GlobalHeader = ({ className, activeSite }) => {
               > li {
                 transition: all 0.2s ease-out;
                 color: var(--secondary-text-color);
-                > a{
-                  font-family: Open Sans,Open Sans,Corbel,Arial,sans-serif;
+                > a {
+                  font-family: Open Sans, Open Sans, Corbel, Arial, sans-serif;
                   font-weight: 400;
                   font-size: 1rem;
                   line-height: 1.625;
@@ -366,19 +364,20 @@ const GlobalHeader = ({ className, activeSite }) => {
           position: relative;
           z-index: 10;
           box-shadow: 0 0.25rem 1.875rem rgb(84 86 90 / 10%);
-         
+
           @media screen and (max-width: ${NAV_BREAKPOINT}) {
             display: flex;
           }
           .dark-mode & {
             background-color: var(--color-neutrals-800);
           }
-          ${isOpen && `
+          ${isOpen &&
+          `
           background-color: var(--color-white);
           `}
         `}
       >
-       <div
+        <div
           css={css`
             display: flex;
             width: 100%;
@@ -392,103 +391,103 @@ const GlobalHeader = ({ className, activeSite }) => {
           `}
         >
           <a
-              href="https://newrelic.com"
-              onClick={useInstrumentedHandler(null, {
+            href="https://newrelic.com"
+            onClick={useInstrumentedHandler(null, {
               eventName: 'externalLinkClick',
               category: 'LinkClick',
               origin: 'globalHeader',
               href: 'https://newrelic.com',
-              })}
-              css={css`
-                display: flex;
-                align-items: center;
-                margin-right: 3rem;
-                height: 100%;
-              
-              `}
-            >
-              
-          <NewRelicLogo
-                  size={hideLogoText ? '24px' : '104px'}
-                  css={css`
-                  width: 7.5rem;
-                    .logo-text {
-                      fill: var(--color-neutrals-900);
-
-                      .dark-mode & {
-                        fill: var(--color-neutrals-100);
-                      }
-                    }
-                  `}
-                  omitText={hideLogoText}
-                />
-                </a>
-      </div>         
-          <div
+            })}
             css={css`
-              display: grid;
-              grid-auto-flow: column;
+              display: flex;
               align-items: center;
-              flex-wrap: wrap;
-              justify-content: space-between;
+              margin-right: 3rem;
+              height: 100%;
             `}
           >
-            <div>
-            <DarkModeToggle
-                css={[
-                  actionIcon,
-                  action,
-                  css`
-                    margin: 27px 0px 27px 19px;
-                    color: var(--color-neutrals-700);
-                    .dark-mode & {
-                      color: var(--color-neutrals-300);
-                    }
+            <NewRelicLogo
+              size={hideLogoText ? '24px' : '104px'}
+              css={css`
+                width: 7.5rem;
+                .logo-text {
+                  fill: var(--color-neutrals-900);
 
-                    @media screen and (max-width: 450px) {
-                      margin: 0;
-                    }
-                  `,
-                ]}
-                size="1.5rem"
-              />
-            </div>
-              <div>
-              <Button
-                css={css`
-                  display: flex;
-                  justify-content: center;
-                  align-items: center;
-                  background: transparent;
-                  border: none;
-                  cursor: pointer;
-                  width: 48;
-                  height: 48;
+                  .dark-mode & {
+                    fill: var(--color-neutrals-100);
+                  }
+                }
+              `}
+              omitText={hideLogoText}
+            />
+          </a>
+        </div>
+        <div
+          css={css`
+            display: grid;
+            grid-auto-flow: column;
+            align-items: center;
+            flex-wrap: wrap;
+            justify-content: space-between;
+          `}
+        >
+          <div>
+            <DarkModeToggle
+              css={[
+                actionIcon,
+                action,
+                css`
+                  margin: 27px 0px 27px 19px;
+                  color: var(--color-neutrals-700);
                   .dark-mode & {
                     color: var(--color-neutrals-300);
                   }
-                  ${isOpen ? `
+
+                  @media screen and (max-width: 450px) {
+                    margin: 0;
+                  }
+                `,
+              ]}
+              size="1.5rem"
+            />
+          </div>
+          <div>
+            <Button
+              css={css`
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                background: transparent;
+                border: none;
+                cursor: pointer;
+                width: 48;
+                height: 48;
+                .dark-mode & {
+                  color: var(--color-neutrals-300);
+                }
+                ${isOpen
+                  ? `
                     color:     color: var(--color-neutrals-700);
-                  ` : `
+                  `
+                  : `
                     color:     color: var(--color-neutrals-700);
                   `}
-                `}
-                title="Toggle menu"
-                onClick={() => setOpen(!isOpen)}
-              >
-                {isOpen ? <X /> : <Menu />}
-              </Button>
-              </div>
+              `}
+              title="Toggle menu"
+              onClick={() => setOpen(!isOpen)}
+            >
+              {isOpen ? <X /> : <Menu />}
+            </Button>
           </div>
-        </div> 
+        </div>
+      </div>
       {isOpen && (
         <div
           css={css`
-            display:block;
+            display: block;
             position: absolute;
             width: 100vw;
             height: 100vh;
-            background-color:var(--color-white);
+            background-color: var(--color-white);
             z-index: 1;
             .dark-mode & {
               background-color: var(--color-neutrals-800);
@@ -544,25 +543,23 @@ const GlobalHeader = ({ className, activeSite }) => {
             </ul>
           </nav>
           <div
-          css={css`
-          display:grid;
-          grid-template-columns: 1fr 1fr;
-          grid-column-gap: 1.5rem;
-          padding: 1.6875rem;
-          width: -webkit-fill-available;
-           bottom: 0;
-           position: fixed;
-           box-shadow: 0 0.25rem 1.875rem rgb(84 86 90 / 10%);
-         
-         ` }
+            css={css`
+              display: grid;
+              grid-template-columns: 1fr 1fr;
+              grid-column-gap: 1.5rem;
+              padding: 1.6875rem;
+              width: -webkit-fill-available;
+              bottom: 0;
+              position: fixed;
+              box-shadow: 0 0.25rem 1.875rem rgb(84 86 90 / 10%);
+            `}
           >
-          
-              <Button
-                as={ExternalLink}
-                size={Button.SIZE.SMALL}
-                variant={Button.VARIANT.LINK}
-                href="https://one.newrelic.com"
-                css={css`
+            <Button
+              as={ExternalLink}
+              size={Button.SIZE.SMALL}
+              variant={Button.VARIANT.LINK}
+              href="https://one.newrelic.com"
+              css={css`
                 ease;
                 transition: background-color 0.3s ease,color 0.3s ease,border-color 0.3s ease;
                 font-family: Open Sans,Open Sans,Corbel,Arial,sans-serif;
@@ -589,45 +586,43 @@ const GlobalHeader = ({ className, activeSite }) => {
 
                   
                 `}
-              >
-                <span>{t('button.login')}</span>
-              </Button>
-           
-            
-              <Button
-                as={ExternalLink}
-                className={className}
-                href="https://newrelic.com/signup"
-                size={Button.SIZE.SMALL}
-                variant={Button.VARIANT.PRIMARY}
-                instrumentation={{
-                  component: 'SignupButton',
-                  layoutElement: 'globalHeader',
-                }}
-                css={css`
+            >
+              <span>{t('button.login')}</span>
+            </Button>
+
+            <Button
+              as={ExternalLink}
+              className={className}
+              href="https://newrelic.com/signup"
+              size={Button.SIZE.SMALL}
+              variant={Button.VARIANT.PRIMARY}
+              instrumentation={{
+                component: 'SignupButton',
+                layoutElement: 'globalHeader',
+              }}
+              css={css`
                 padding: 0.375rem 1rem;
-                    transition: background-color 0.3s ease,color 0.3s ease,border-color 0.3s ease;
-                    font-family: Open Sans,Open Sans,Corbel,Arial,sans-serif;
-                    font-weight: 400;
-                    font-size: 1rem;
-                    line-height: 1.625;
-                    text-decoration: none;
-                    border: 1px solid transparent;
-                    border-radius: 4px;
-                    cursor: pointer;
-                    display: inline-block;
-                    text-align: center;
-                    vertical-align: middle;
-                    white-space: nowrap;
-                    background-color: #00838f;
-                    color: #fff;
-                    border-color: #00838f;
-                  
-                `}
-              >
-                <span>{t('button.signUp')}</span>
-              </Button>
-           
+                transition: background-color 0.3s ease, color 0.3s ease,
+                  border-color 0.3s ease;
+                font-family: Open Sans, Open Sans, Corbel, Arial, sans-serif;
+                font-weight: 400;
+                font-size: 1rem;
+                line-height: 1.625;
+                text-decoration: none;
+                border: 1px solid transparent;
+                border-radius: 4px;
+                cursor: pointer;
+                display: inline-block;
+                text-align: center;
+                vertical-align: middle;
+                white-space: nowrap;
+                background-color: #00838f;
+                color: #fff;
+                border-color: #00838f;
+              `}
+            >
+              <span>{t('button.signUp')}</span>
+            </Button>
           </div>
         </div>
       )}
