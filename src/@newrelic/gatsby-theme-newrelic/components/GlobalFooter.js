8271061c3b35577ed49161cff86722f2510bacf4
Joe Gregory <jgregory@newrelic.com>
2022-04-26 16:59:57 -0700

    chore: footer links per design spec @ 1440px width

--- src/@newrelic/gatsby-theme-newrelic/components/GlobalFooter.js
+++ src/@newrelic/gatsby-theme-newrelic/components/GlobalFooter.js
@@ -1,21 +1,59 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Button from '@newrelic/gatsby-theme-newrelic/src/components/Button';
-import Logo from '@newrelic/gatsby-theme-newrelic/src/components/Logo';
-import Icon from '@newrelic/gatsby-theme-newrelic/src/components/Icon';
-import ExternalLink from '@newrelic/gatsby-theme-newrelic/src/components/ExternalLink';
 import { graphql, useStaticQuery } from 'gatsby';
+
+import ExternalLink from '@newrelic/gatsby-theme-newrelic/src/components/ExternalLink';
+import Link from '@newrelic/gatsby-theme-newrelic/src/components/Link';
+import Logo from '@newrelic/gatsby-theme-newrelic/src/components/Logo';
+import PropTypes from 'prop-types';
+import React from 'react';
 import { css } from '@emotion/react';
 import useThemeTranslation from '@newrelic/gatsby-theme-newrelic/src//hooks/useThemeTranslation';
-import Trans from '@newrelic/gatsby-theme-newrelic/src/components/Trans';
-import Link from '@newrelic/gatsby-theme-newrelic/src/components/Link';
-import useLocale from '@newrelic/gatsby-theme-newrelic/src//hooks/useLocale';
 
-// We need to use this as a JS value otherwise the HTML entity gets saved in the
-// string and escaped by React, therefore rendering the literal &copy; text in
-// the footer
-const copyrightSymbol = String.fromCharCode(169);
-const year = new Date().getFullYear();
+const RESOURCES = [
+  {
+    title: 'About Us',
+    href: 'https://newrelic.com/about',
+  },
+  {
+    title: 'Leadership',
+    href: 'https://newrelic.com/about/leadership',
+  },
+  {
+    title: 'Careers',
+    href: 'https://newrelic.com/about/culture',
+  },
+  {
+    title: 'Social Impact',
+    href: 'https://newrelic.com/social-impact',
+  },
+  {
+    title: 'Newsroom',
+    href: 'https://newrelic.com/about/newsroom',
+  },
+  {
+    title: 'Customers',
+    href: 'https://newrelic.com/customers',
+  },
+  {
+    title: 'Partner Program',
+    href: 'https://newrelic.com/solutions/partners',
+  },
+  {
+    title: 'Investor Relations',
+    href: 'https://ir.newrelic.com/investors-home/default.aspx',
+  },
+  {
+    title: 'Suppliers Portal',
+    href: 'https://newrelic.com/procurement/suppliers',
+  },
+  {
+    title: 'Security',
+    href: 'https://newrelic.com/security',
+  },
+  {
+    title: 'Contact Us',
+    href: 'https://newrelic.com/about/contact-us',
+  },
+];
 
 const GlobalFooter = ({ className }) => {
   const { t } = useThemeTranslation();
@@ -31,145 +69,92 @@ const GlobalFooter = ({ className }) => {
       }
     }
   `);
-  const { locale } = useLocale();
-
-  const { siteMetadata } = site;
 
   return (
     <footer
       data-swiftype-index={false}
       className={className}
       css={css`
+        --background-color: #1d252c;
+        --secondary-text-color: #898e91;
+
         width: 100%;
         color: var(--secondary-text-color);
-        background-color: var(--color-neutrals-050);
-        z-index: 1;
-
-        .dark-mode & {
-          background-color: var(--color-dark-050);
-        }
+        background-color: var(--background-color);
 
         a {
           color: currentColor;
+          text-decoration: none;
+        }
+
+        a:hover {
+          text-decoration: underline;
         }
       `}
     >
-      <div
-        css={css`
-          font-size: 0.75rem;
-          align-items: center;
-          justify-content: space-between;
-          display: flex;
-          padding: 1rem var(--site-content-padding);
-          max-width: var(--site-max-width);
-          margin: 0 auto;
-
-          @media screen and (max-width: 550px) {
-            flex-direction: column;
-            justify-content: center;
-          }
-        `}
-      >
-        <Link to="/">
-          <Logo
-            width="150px"
-            css={css`
-              display: block;
-
-              @media screen and (max-width: 550px) {
-                margin-bottom: 1rem;
-              }
-            `}
-          />
-        </Link>
-        <div>
-          <Button
-            as={ExternalLink}
-            variant={Button.VARIANT.OUTLINE}
-            to="https://developer.newrelic.com/contribute-to-quickstarts/"
-            instrumentation={{
-              component: 'GlobalFooter',
-              eventName: 'instantObservability',
-              category: 'BuildYourOwnQuickstartClick',
-            }}
-          >
-            <Icon
-              name="zap"
-              css={css`
-                margin-right: 7px;
-              `}
-            />
-            Build your own
-            <Icon
-              name="external-link"
-              css={css`
-                margin-left: 7px;
-              `}
-            />
-          </Button>
-        </div>
-      </div>
-
-      <div
-        css={css`
-          background-color: rgba(0, 0, 0, 0.05);
-
-          .dark-mode & {
-            background-color: rgba(0, 0, 0, 0.2);
-          }
-        `}
-      >
+      <div>
         <div
           css={css`
-            font-size: 0.75rem;
-            align-items: center;
-            justify-content: space-between;
+            --default-first-column-size: 1024px;
             display: grid;
-            grid-template-columns: auto auto;
-            grid-template-areas: 'copyright legal';
-            padding: 0.5rem var(--site-content-padding);
-            max-width: var(--site-max-width);
-            margin: 0 auto;
+            grid-template-columns: var(--default-first-column-size) auto;
+
+            grid-template-areas:
+              'resources socials'
+              'logo logo'
+              'legal locale';
 
             @media screen and (max-width: 760px) {
-              justify-content: center;
+              ${
+                /*justify-content: center;
               text-align: center;
               grid-template-columns: auto;
               grid-gap: 0.5rem;
               grid-template-areas:
                 'legal'
                 'copyright';
+              */ ''
+              }
+              ${/* TODO */ ``}
             }
           `}
         >
-          <Trans
-            i18nKey="footer.copyright"
-            parent="div"
-            css={css`
-              grid-area: copyright;
-              text-transform: uppercase;
-              font-size: 0.5rem;
-              letter-spacing: 0.1rem;
-            `}
-          >
-            Copyright {{ copyrightSymbol }} {{ year }} New Relic Inc.
-          </Trans>
           <div
             css={css`
-              display: flex;
-              flex-wrap: wrap;
-              justify-content: center;
-              grid-area: legal;
+              margin: 64px 0px;
+              justify-content: space-around;
+              display: grid;
+              grid-template-rows: repeat(4, 1fr);
+              grid-auto-flow: column;
+
+              grid-area: resources;
 
               a {
-                margin-left: 0.75rem;
-                white-space: nowrap;
+                font-size: 18px;
+                line-height: 24px;
+                margin-top: 32px;
               }
             `}
           >
-            <ExternalLink href="https://newrelic.com/about/careers">
-              {t('footer.careers', 'Careers')}
-            </ExternalLink>
+            {RESOURCES.map((resource) => (
+              <ExternalLink href={resource.href}>{resource.title}</ExternalLink>
+            ))}
+          </div>
+
+          <div
+            css={css`
+              display: flex;
+              margin-left: 104px;
+
+              justify-content: flex-start;
+              grid-area: legal;
+
+              a {
+                white-space: nowrap;
+                margin-right: 32px;
+              }
+            `}
+          >
             {sitePage ? (
               <Link to="/terms">{t('footer.terms', 'Terms of Service')}</Link>
             ) : (
@@ -189,6 +174,27 @@ const GlobalFooter = ({ className }) => {
             </ExternalLink>
             <ExternalLink href="https://newrelic.com/termsandconditions/uk-slavery-act">
               {t('footer.ukSlaveryAct', 'UK Slavery Act')}
+            </ExternalLink>
+          </div>
+          <div
+            css={css`
+              display: flex;
+              margin-right: 104px;
+
+              justify-content: flex-end;
+              grid-area: locale;
+
+              a {
+                margin-right: 32px;
+                white-space: nowrap;
+              }
+            `}
+          >
+            <ExternalLink href="https://newrelic/com/de">
+              {t('footer.francais', 'Francais')}
+            </ExternalLink>
+            <ExternalLink href="https://newrelic/com/de">
+              {t('footer.deutsch', 'Deutsch')}
             </ExternalLink>
           </div>
         </div>
