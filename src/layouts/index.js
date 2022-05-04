14bca219d06eaca8433fc1e4ad3b338fe466e6a1
Tabatha Zeitke <tzeitke@newrelic.com>
2022-05-04 16:38:46 -0700

    feat: set theme styles to always be light-mode

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,6 +4,11 @@ import PropTypes from 'prop-types';
 import GlobalStyles from '../components/GlobalStyles';
 
 const Layout = ({ children, pageContext }) => {
+  //override HTML body class to always be 'light-mode' until site has dark-mode design
+  const pageBodyClass = document.body.classList;
+  if (pageBodyClass.contains('dark-mode')) {
+    pageBodyClass.replace('dark-mode', 'light-mode');
+  }
   if (pageContext.layout === 'QuickStartLayout') {
     return (
       <QuickStartLayout>
