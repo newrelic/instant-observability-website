fa0bbf050522d73ef65fee3893c3401cca616a5f
Tabatha Zeitke <tzeitke@newrelic.com>
2022-05-04 17:12:01 -0700

    move theme check into useEffect

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,10 +5,6 @@ import GlobalStyles from '../components/GlobalStyles';
 
 const Layout = ({ children, pageContext }) => {
   //override HTML body class to always be 'light-mode' until site has dark-mode design
-  const pageBodyClass = document.body.classList;
-  if (pageBodyClass.contains('dark-mode')) {
-    pageBodyClass.replace('dark-mode', 'light-mode');
-  }
   if (pageContext.layout === 'QuickStartLayout') {
     return (
       <QuickStartLayout>
