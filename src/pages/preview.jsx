199515f0446ed9484b95a5334386163910a66b76
Joe Gregory <jgregory@newrelic.com>
2022-05-12 12:04:36 -0700

    chore: get files from quickstart using Github API

--- src/pages/preview.jsx
+++ src/pages/preview.jsx
@@ -3,48 +3,53 @@ import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 const GITHUB_API_BASE_URL =
-  'https://api.github.com/repos/newrelic/newrelic-quickstarts/contents/quickstarts';
+  'https://api.github.com/repos/newrelic/newrelic-quickstarts/contents';
 
 const GITHUB_API_PULL_URL =
   'https://api.github.com/repos/newrelic/newrelic-quickstarts/pulls';
+
+/**
+ * Recursive function to walk the file system in GitHub
+ * @param {String} url - GitHub API URL to walk the file system
+ * @returns {Array} array of files
+ **/
+const iterateDirs = async (files, url) => {
+  const branchResponse = await fetch(url);
+  const branchResponseJSON = await branchResponse.json();
+
+  for (const dirListing of branchResponseJSON) {
+    if (dirListing.type === 'file') {
+      files.push(dirListing);
+    } else {
+      await iterateDirs(files, dirListing.url);
+    }
+  }
+};
+
+const fetchData = async (prNumber, quickstartPath) => {
+  // Hit the Github API for SHA that references the PR branch
+  const prResponse = await fetch(`${GITHUB_API_PULL_URL}/${prNumber}`);
+  const prResponseJSON = await prResponse.json();
+  const branchSHA = prResponseJSON.head.sha;
+
+  // Recursively walk the Github API from the root of the quickstart
+  const files = [];
+  await iterateDirs(
+    files,
+    `${GITHUB_API_BASE_URL}/quickstarts/${quickstartPath}?ref=${branchSHA}`
+  );
+  console.log(files);
+};
 
 const PreviewPage = ({ location }) => {
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
     const prNumber = urlParams.get('pr');
+    const quickstartPath = urlParams.get('quickstart');
 
-    const fetchData = async () => {
-      const prResponse = await fetch(
-        `${GITHUB_API_PULL_URL}/${prNumber}/files`
-      );
-      const prResponseJSON = await prResponse.json();
-
-      // In this scenario, we get the quickstart path from the filepath of a dashboard
-      // This scenario also works with alerts
-      const fileName = prResponseJSON[1].filename
-        .split('quickstarts/')[1]
-        .split('dashboards/');
-
-      // In this scenario, we get the quickstart path from a file path at the root
-      // of the quickstart directory
-      const fileNameScenario2 = prResponseJSON[0].filename.split(
-        'quickstarts/'
-      );
-
-      // This gives us the branch SHA by using the raw_url in respones JSON
-      // It aint pretty, but its honest work.
-      const branchSHA = prResponseJSON[0].raw_url
-        .split('raw/')[1]
-        .split('/')[0];
-
-      console.log(branchSHA);
-    };
-
-    fetchData();
+    fetchData(prNumber, quickstartPath);
   }, []);
 
-  // console.log(prNumber);
-  // console.log(quickstartPath);
   return <span>oh hai</span>;
 };
 
