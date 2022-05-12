069d19c76a27a26f16e00db6291253edcd353c79
Joe Gregory <jgregory@newrelic.com>
2022-05-11 17:01:58 -0700

    chore: grab quickstart filepath and SHA
    * Ability to grab both the quickstart filepath and SHA from one fetch
      request

--- src/pages/preview.jsx
+++ src/pages/preview.jsx
@@ -1,12 +1,48 @@
+import React, { useEffect } from 'react';
+
 import PropTypes from 'prop-types';
-import useQueryParameter from '../hooks/useQueryParameter';
+
+const GITHUB_API_BASE_URL =
+  'https://api.github.com/repos/newrelic/newrelic-quickstarts/contents/quickstarts';
+
+const GITHUB_API_PULL_URL =
+  'https://api.github.com/repos/newrelic/newrelic-quickstarts/pulls';
 
 const PreviewPage = ({ location }) => {
-  const prNumber = useQueryParameter('pr', location);
-  const quickstartPath = useQueryParameter('quickstart', location);
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const prNumber = urlParams.get('pr');
 
-  console.log(prNumber);
-  console.log(quickstartPath);
+    const fetchData = async () => {
+      const prResponse = await fetch(
+        `${GITHUB_API_PULL_URL}/${prNumber}/files`
+      );
+      const prResponseJSON = await prResponse.json();
+
+      // In this scenario, we get the quickstart path from the filepath of a dashboard
+      // This scenario also works with alerts
+      const fileName = prResponseJSON[1].filename
+        .split('quickstarts/')[1]
+        .split('dashboards/');
+
+      // In this scenario, we get the quickstart path from a file path at the root
+      // of the quickstart directory
+      const fileNameScenario2 = prResponseJSON[0].filename.split(
+        'quickstarts/'
+      );
+
+      const branchSHA = prResponseJSON[0].raw_url
+        .split('raw/')[1]
+        .split('/')[0];
+
+      console.log(branchSHA);
+    };
+
+    fetchData();
+  }, []);
+
+  // console.log(prNumber);
+  // console.log(quickstartPath);
   return <span>oh hai</span>;
 };
 
