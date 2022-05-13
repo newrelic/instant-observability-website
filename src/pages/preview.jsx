c1ec63b8618c006d8a1af655f4d5eb8d4678e1fa
Joe Gregory <jgregory@newrelic.com>
2022-05-12 17:27:53 -0700

    chore: create content object for parsing files

--- src/pages/preview.jsx
+++ src/pages/preview.jsx
@@ -10,8 +10,8 @@ const GITHUB_API_PULL_URL =
   'https://api.github.com/repos/newrelic/newrelic-quickstarts/pulls';
 
 /**
- * Recursive function to walk the file system in GitHub
- * @param {String} url - GitHub API URL to walk the file system
+ * Recursive function to walk the file system in Github
+ * @param {String} url - Github API URL to walk the file system
  * @returns {Array} array of files
  **/
 const iterateDirs = async (url) => {
@@ -30,13 +30,62 @@ const iterateDirs = async (url) => {
   return fileAggregator;
 };
 
+const getFileType = (fileName) => {
+  // Regex for different filetypes
+  const imageFileTypes = /^.*\.(jpg|jpeg|svg|png)$/i;
+  const yamlFileTypes = /^.*\.(yaml|yml)$/i;
+  const jsonFileType = /^.*\.(json)$/i;
+
+  if (fileName.match(imageFileTypes)) {
+    return 'image';
+  } else if (fileName.match(yamlFileTypes)) {
+    return 'yaml';
+  } else if (fileName.match(jsonFileType)) {
+    return 'json';
+  }
+};
+
+const determineContent = async ({ download_url, name: fileName }) => {
+  const rawFileResponse = await fetch(download_url);
+  const fileType = getFileType(fileName);
+  const content =
+    fileType === 'image'
+      ? await rawFileResponse.blob()
+      : await rawFileResponse.text();
+
+  const rawContentObj = {
+    type: fileType,
+    fileName,
+    content,
+  };
+  return rawContentObj;
+};
+
+/**
+ * Async function grabs the raw content from files
+ * @param {Array} fileAggregator - array of Github metadata objects
+ * @returns {Array<Object>} - array of objects containg raw content to parse
+ *
+ **/
+const getRawContent = async (fileAggregator) => {
+  const rawContent = Promise.all(
+    fileAggregator.map(async (rawMetadata) => {
+      return determineContent(rawMetadata);
+    })
+  );
+
+  return rawContent;
+};
+
+/**
+ * Async function handles fetching changed files in pull request from Github
+ **/
 const getQuickstartFilesFromPR = async (prNumber, quickstartPath) => {
   // Hit the Github API for SHA that references the PR branch
   const prResponse = await fetch(`${GITHUB_API_PULL_URL}/${prNumber}`);
 
   if (prResponse.status !== 200 || !prResponse.ok) {
-    await navigate('/');
-    return;
+    throw `Response from github came back with status: ${prResponse.status}`;
   }
 
   const prResponseJSON = await prResponse.json();
@@ -46,7 +95,9 @@ const getQuickstartFilesFromPR = async (prNumber, quickstartPath) => {
   const fileAggregator = await iterateDirs(
     `${GITHUB_API_BASE_URL}/quickstarts/${quickstartPath}?ref=${branchSHA}`
   );
-  console.log(fileAggregator);
+
+  const rawContent = await getRawContent(fileAggregator);
+  console.log(rawContent);
 };
 
 const PreviewPage = ({ location }) => {
@@ -60,7 +111,12 @@ const PreviewPage = ({ location }) => {
       return;
     }
 
-    getQuickstartFilesFromPR(prNumber, quickstartPath);
+    try {
+      getQuickstartFilesFromPR(prNumber, quickstartPath);
+    } catch (error) {
+      navigate('/');
+      return;
+    }
   }, []);
 
   return <span>oh hai</span>;
