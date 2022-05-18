9020071fa38477d0ef66ba35b477ae68043d7e45
Joe Gregory <jgregory@newrelic.com>
2022-05-18 10:26:08 -0700

    chore: added more context for template quickstart

--- src/pages/preview.jsx
+++ src/pages/preview.jsx
@@ -7,21 +7,28 @@ import useLocalhostQuickstart from '../hooks/useLocalhostQuickstart';
 const testQuickstart = {
   title: 'Title of quickstart',
   slug: 'Slug of quickstart',
-  description: "Description of quickstart",
-  packUrl: 'https://github.com/newrelic/newrelic-quickstarts/tree/main/quickstarts/battlesnake',
+  description: 'Description of quickstart',
+  packUrl:
+    'https://github.com/newrelic/newrelic-quickstarts/tree/main/quickstarts/battlesnake',
   id: '63a65857-a25e-4654-9174-928e5894b3b9',
   level: 'NEW_RELIC',
   logoUrl:
-    'https://raw.githubusercontent.com/newrelic/newrelic-quickstarts/main/quickstarts/oma-aqm/logo.png',
-  summary:
-    'summary of quickstart',
+    'https://raw.githubusercontent.com/newrelic/newrelic-quickstarts/main/_template/logo.svg',
+  summary: 'summary of quickstart',
   websiteUrl: null,
   keywords: [],
-  authors: [
-    'Joe Gregory (New Relic)',
-  ],
+  authors: ['Joe Gregory (New Relic)'],
   relatedResources: [],
-  dashboards: [],
+  dashboards: [
+    {
+      name: 'Battlesnake Performance',
+      description:
+        'Monitor your Battlensakes performance and see how it stacks up against your competition!',
+      screenshots: [
+        'https://raw.githubusercontent.com/newrelic/newrelic-quickstarts/main/quickstarts/battlesnake/dashboards/battlesnake-performance/battlesnake-performance.png',
+      ],
+    },
+  ],
   alerts: [],
   documentation: [],
   installPlans: [
@@ -37,14 +44,14 @@ const PreviewPage = ({ location }) => {
   let contentFiles;
 
   if (urlParams.get('local')) {
-    contentFiles = useLocalhostQuickstart(location);
+    //    contentFiles = useLocalhostQuickstart(location);
   } else {
     //contentFiles = usePullRequestQuickstart(location);
   }
 
   console.log('Parsed quickstart content:', contentFiles);
 
-  const data = { quickstarts: testQuickstart}
+  const data = { quickstarts: testQuickstart };
 
   return <QuickstartDetails data={data} location={location} />;
 };
