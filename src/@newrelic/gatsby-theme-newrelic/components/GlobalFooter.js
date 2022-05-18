9fb68b1ed1ebf9fde927cd15f349d318b1f9846e
pkosanam <pkosanam@newrelic.com>
2022-05-18 23:07:46 +0530

    NR-10893 fix social icons alignment as home page

--- src/@newrelic/gatsby-theme-newrelic/components/GlobalFooter.js
+++ src/@newrelic/gatsby-theme-newrelic/components/GlobalFooter.js
@@ -34,6 +34,34 @@ const GlobalFooter = ({ className }) => {
     }
   `);
 
+  // these icons will be hidden on mobile view
+  const renderSocialIcons = () => {
+    return (
+      SOCIALS.map((social) => (
+        <ExternalLink
+          key={social.title}
+          href={social.href}
+          css={css`
+            @media screen and (max-width: ${MOBILE_BREAKPOINT}) {
+              display: none;
+            }
+          `}
+        >
+          <Icon
+            name={social.title}
+            size="24px"
+            css={css`
+              outline: none;
+              stroke-width: 0px;
+              color: var(--secondary-text-color);
+              fill: var(--secondary-text-color);
+            `}
+          />
+        </ExternalLink>
+      ))
+    )
+  }
+
   return (
     <footer
       data-swiftype-index={false}
@@ -172,7 +200,7 @@ const GlobalFooter = ({ className }) => {
 
                 > span {
                   grid-column: span 7;
-                  margin-bottom: 14px;
+                  margin-bottom: 2rem;
                 }
 
                 > a {
@@ -183,20 +211,41 @@ const GlobalFooter = ({ className }) => {
           >
             <span>Follow us</span>
 
-            {SOCIALS.map((social) => (
-              <ExternalLink key={social.title} href={social.href}>
-                <Icon
-                  name={social.title}
-                  size="24px"
-                  css={css`
-                    outline: none;
-                    stroke-width: 0px;
-                    color: var(--secondary-text-color);
-                    fill: var(--secondary-text-color);
-                  `}
-                />
-              </ExternalLink>
-            ))}
+            {/* hide icons on mobile view and display them on large screens */}
+            {renderSocialIcons()}
+
+            {/* display icons on mobile view and hide on large screens */}
+            <div
+              css={css`
+                display: none;
+                @media screen and (max-width: ${MOBILE_BREAKPOINT}) {
+                  padding: 0;
+                  row-gap: 2rem;
+                  column-gap: 1.5rem;
+                  flex-wrap: wrap;
+                  flex-direction: row;
+                  max-width: 10.5rem;
+                  display: flex;
+                  margin-bottom: 1rem;
+                  justify-content: flex-start;
+                }
+              `}
+            >
+              {SOCIALS.map((social) => (
+                <ExternalLink key={social.title} href={social.href}>
+                  <Icon
+                    name={social.title}
+                    size="24px"
+                    css={css`
+                      outline: none;
+                      stroke-width: 0px;
+                      color: var(--secondary-text-color);
+                      fill: var(--secondary-text-color);
+                    `}
+                  />
+                </ExternalLink>
+              ))}
+            </div>
           </div>
           <div
             css={css`
@@ -290,11 +339,12 @@ const GlobalFooter = ({ className }) => {
           </div>
           <div
             css={css`
-              margin: 32px 0px 20px 0px;
+              margin-top: 1.25rem;
               font-size: 0.875rem;
               line-height: 1.25rem;
               @media screen and (max-width: ${MOBILE_BREAKPOINT}) {
-                margin: 32px 0px 20px 40px;
+                margin-top: 2rem;
+                margin-left: 40px;
               }
             `}
           >
