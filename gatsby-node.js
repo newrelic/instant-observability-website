0967c82ca9afdb6e4579d689095c1af32947914a
Mickey Ryan <mickeyryan@newrelic.com>
2022-06-27 13:04:06 -0400

    Fix lint error

--- gatsby-node.js
+++ gatsby-node.js
@@ -92,10 +92,10 @@ exports.onCreateWebpackConfig = ({ actions, plugins }) => {
         zlib: false,
       },
       alias: {
-        "@components": path.resolve(__dirname, 'src/components/'),
-        "@hooks": path.resolve(__dirname, 'src/hooks/'),
-        "@utils": path.resolve(__dirname, 'src/utils/'),
-        "@layouts": path.resolve(__dirname, 'src/layouts/'),
+        '@components': path.resolve(__dirname, 'src/components/'),
+        '@hooks': path.resolve(__dirname, 'src/hooks/'),
+        '@utils': path.resolve(__dirname, 'src/utils/'),
+        '@layouts': path.resolve(__dirname, 'src/layouts/'),
       },
     },
   });
