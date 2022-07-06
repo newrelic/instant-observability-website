0b2c5a0594898080386cfa56560331d65f925f00
Joe Gregory <jgregory@newrelic.com>
2022-07-06 15:02:16 -0700

    chore: lint fix

--- src/components/WhatsIncluded/Dashboards.js
+++ src/components/WhatsIncluded/Dashboards.js
@@ -3,12 +3,8 @@ import { quickstart } from '../../types';
 import QuickstartDashboards from '../QuickstartDashboards';
 import { css } from '@emotion/react';
 import EmptyTab from '../EmptyTab';
-import Share from '@components/Share';
 import { useLocation } from '@reach/router';
-import {
-  QUICKSTARTS_COLLAPSE_BREAKPOINT,
-  MIN_WIDTH_BREAKPOINT,
-} from '@data/constants';
+import { QUICKSTARTS_COLLAPSE_BREAKPOINT } from '@data/constants';
 
 const Dashboards = ({ quickstart }) => {
   const location = useLocation();
