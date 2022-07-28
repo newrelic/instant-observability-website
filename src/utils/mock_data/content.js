87c57c17881fd80dd02e524c99180ea6134aa79b
Tabatha Zeitke <tzeitke@newrelic.com>
2022-07-28 13:25:41 -0700

    chore: update test mock data

--- src/utils/mock_data/content.js
+++ src/utils/mock_data/content.js
@@ -135,9 +135,11 @@ export const expectedDashboardOutput = [
   {
     name: 'mock dashboard name',
     description: 'mock dashboard description',
-    screenshots: [
-      'mock/url/for/mock_dashboard01.png',
-      'mock/url/for/mock_dashboard02.png',
-    ],
+    screenshots: {
+      publicURL: [
+        'mock/url/for/mock_dashboard01.png',
+        'mock/url/for/mock_dashboard02.png',
+      ],
+    },
   },
 ];
