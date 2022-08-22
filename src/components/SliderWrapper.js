535cf1f9b5da731fda1e3705ecf6d80378642cd9
Mickey Ryan <mickeyryan@newrelic.com>
2022-08-22 16:39:14 -0400

    Move super tiles to be prop

--- src/components/SliderWrapper.js
+++ src/components/SliderWrapper.js
@@ -11,14 +11,19 @@ import shuffleArray from '@utils/shuffleArray';
 // Types
 import { quickstart } from '../types';
 
-const SliderWrapper = ({ indexSettings, quickstarts, category, showSuperTiles }) => {
+const SliderWrapper = ({
+  indexSettings,
+  quickstarts,
+  category,
+  showSuperTiles,
+}) => {
   /* 
     The following line is disabled because including `quickstarts` in the dependencies or not 
     including the empty dep array, we run into issues with an onHover event from react-slick 
     which causes the tiles to re-render.  In this case, re-render also causes a shuffle of the 
     components which we don't want.
   */
-  const shuffledQuickstarts = useMemo(() => shuffleArray(quickstarts), []); // eslint-disable-line react-hooks/exhaustive-deps 
+  const shuffledQuickstarts = useMemo(() => shuffleArray(quickstarts), []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <Slider {...indexSettings}>
