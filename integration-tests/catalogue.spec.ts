ed8154e66c53433ac0334377c9a902ca21c95432
Alec Swanson <aswanson@newrelic.com>
2022-09-09 13:05:45 -0700

    chore: more tests

--- integration-tests/catalogue.spec.ts
+++ integration-tests/catalogue.spec.ts
@@ -1,54 +1,51 @@
 import { test, expect } from '@playwright/test';
 
-test('go to catalogue page and select guided install', async ({ page }) => {
-  await page.goto('/');
+test.describe('Catalogue page', () => {
+  test('select guided install', async ({ page }) => {
+    await page.goto('/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Instant Observability/);
+    // create a locator
+    const installNR = page.locator('button', { hasText: 'Install New Relic' });
 
-  // create a locator
-  const installNR = page.locator('button', { hasText: 'Install New Relic' });
+    // Click the get started link.
+    await installNR.first().click();
 
-  // Click the get started link.
-  await installNR.first().click();
+    // Expects the URL to contain intro.
+    await expect(page).toHaveURL('https://newrelic.com/signup');
+  });
 
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL('https://newrelic.com/signup');
-});
+  test('select a category', async ({ page }) => {
+    await page.goto('/');
 
-test('go to catalogue page and select a category', async ({ page }) => {
-  await page.goto('/');
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Instant Observability/);
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Instant Observability/);
+    // create a locator
+    const apmCategory = page.locator(
+      'button >> text=Application monitoring >> visible=true'
+    );
 
-  // create a locator
-  const apmCategory = page.locator(
-    'button >> text=Application monitoring >> visible=true'
-  );
+    // Click the get started link.
+    await apmCategory.click();
 
-  // Click the get started link.
-  await apmCategory.click();
+    // Expects the URL to contain intro.
+    await expect(page).toHaveURL(/.+?category=application-monitoring/);
+  });
 
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.+?category=application-monitoring/);
-});
+  test('enter a search term, hit enter', async ({ page }) => {
+    await page.goto('/');
 
-test('go to catalogue page, enter a search term, hit enter', async ({
-  page,
-}) => {
-  await page.goto('/');
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Instant Observability/);
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Instant Observability/);
+    // create a locator
+    const searchInput = page.locator('input#catalogueSearch');
 
-  // create a locator
-  const searchInput = page.locator('input#catalogueSearch');
+    // Click the get started link.
+    await searchInput.fill('python');
+    await searchInput.press('Enter');
 
-  // Click the get started link.
-  await searchInput.fill('python');
-  await searchInput.press('Enter');
-
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.+?search=python/);
+    // Expects the URL to contain intro.
+    await expect(page).toHaveURL(/.+?search=python/);
+  });
 });
--- integration-tests/landing.spec.ts
+++ integration-tests/landing.spec.ts
@@ -1,54 +1,193 @@
 import { test, expect } from '@playwright/test';
 
-test('go to landing page', async ({ page }) => {
-  await page.goto('/');
+const LANDING_PAGE_URL_PATH = '/node-js/01fdea36-5a15-44b4-a864-c4c99866735b';
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Instant Observability/);
+test.describe('Landing page', () => {
+  test('Header links to newrelic home, platform, pricing, solutions, docs, help center, log in', async ({
+    page,
+  }) => {
+    await page.goto(LANDING_PAGE_URL_PATH);
 
-  // create a locator
-  const installNR = page.locator('button', { hasText: 'Install New Relic' });
+    // Using Xpaths here because the location of these elements should not change
+    const nrLogoLink = page.locator(
+      '//*[@id="gatsby-focus-wrapper"]/div[1]/div/a'
+    );
+    const platformLink = page.locator(
+      '//*[@id="gatsby-focus-wrapper"]/div[1]/div/nav/ul/li[1]/a',
+      { hasText: 'Platform' }
+    );
+    const pricingLink = page.locator(
+      '//*[@id="gatsby-focus-wrapper"]/div[1]/div/nav/ul/li[2]/a',
+      { hasText: 'Pricing' }
+    );
+    const solutionsLink = page.locator(
+      '//*[@id="gatsby-focus-wrapper"]/div[1]/div/nav/ul/li[3]/a',
+      { hasText: 'Solutions' }
+    );
+    const docsLink = page.locator(
+      '//*[@id="gatsby-focus-wrapper"]/div[1]/div/nav/ul/li[4]/a',
+      { hasText: 'Docs' }
+    );
+    const helpCenterLink = page.locator(
+      '//*[@id="gatsby-focus-wrapper"]/div[1]/div/nav/ul/li[5]/a',
+      { hasText: 'Help Center' }
+    );
+    const loginLink = page.locator(
+      '//*[@id="gatsby-focus-wrapper"]/div[1]/div/ul/li/a',
+      { hasText: 'Log in' }
+    );
 
-  // Click the get started link.
-  await installNR.first().click();
+    expect(await nrLogoLink.getAttribute('href')).toEqual(
+      'https://newrelic.com'
+    );
+    expect(await platformLink.getAttribute('href')).toEqual(
+      'https://newrelic.com/platform'
+    );
+    expect(await pricingLink.getAttribute('href')).toEqual(
+      'https://newrelic.com/pricing'
+    );
+    expect(await solutionsLink.getAttribute('href')).toEqual(
+      'https://newrelic.com/devops'
+    );
+    expect(await docsLink.getAttribute('href')).toEqual(
+      'https://docs.newrelic.com/'
+    );
+    expect(await helpCenterLink.getAttribute('href')).toEqual(
+      'https://discuss.newrelic.com/'
+    );
+    expect(await loginLink.getAttribute('href')).toEqual(
+      'https://one.newrelic.com'
+    );
+  });
 
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL('https://newrelic.com/signup');
+  test('Get started is not in the header', async ({ page }) => {
+    await page.goto(LANDING_PAGE_URL_PATH);
+
+    const getStartedLink = page.locator(
+      '//*[@id="gatsby-focus-wrapper"]/div[1]/div/ul/li[2]/a',
+      { hasText: 'Get Started' }
+    );
+
+    expect(await getStartedLink.count()).toEqual(0);
+  });
+
+  test('Top CTA links to the signup page in a new tab', async ({
+    page,
+    context,
+  }) => {
+    await page.goto(LANDING_PAGE_URL_PATH);
+
+    const cta = page.locator('a', { hasText: 'Install now' }).first();
+
+    await cta.click();
+    await page.waitForTimeout(500); // wait for the new tab to open
+    const pages = context.pages();
+
+    await expect(pages[1]).toHaveURL('https://newrelic.com/signup');
+  });
+
+  test('Bread crumb links back to catalogue page', async ({ page }) => {
+    await page.goto(LANDING_PAGE_URL_PATH);
+
+    const breadcrumb = page.locator('//*[@aria-label="breadcrumb"]/span/a');
+
+    //const link = breadcrumb.locator('a');
+    await breadcrumb.click();
+
+    await expect(page).toHaveURL(/.+\/$/);
+  });
 });
 
-test('go to catalogue page and select a category', async ({ page }) => {
-  await page.goto('/');
+test.describe('Landing page with `ajs_user_id` cookie set', () => {
+  test('CTA links to the product for installation', async ({
+    page,
+    context,
+  }) => {
+    context.addCookies([
+      {
+        name: 'ajs_user_id',
+        value: 'testcontent',
+        url: 'http://localhost',
+      },
+    ]);
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Instant Observability/);
+    await page.goto(LANDING_PAGE_URL_PATH);
 
-  // create a locator
-  const apmCategory = page.locator(
-    'button >> text=Application monitoring >> visible=true'
-  );
+    const cta = page.locator('a', { hasText: 'Install now' }).first();
 
-  // Click the get started link.
-  await apmCategory.click();
+    await cta.click();
+    await page.waitForTimeout(1000); // wait for the new tab to open
+    const pages = context.pages();
 
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.+?category=application-monitoring/);
+    // Since we are not logged into NR1, this will redirect to the login page
+    await expect(pages[1]).toHaveURL(/https:\/\/login\.newrelic.com\/login/);
+  });
 });
 
-test('go to catalogue page, enter a search term, hit enter', async ({
-  page,
-}) => {
-  await page.goto('/');
+test.describe('Landing page with `utm_medium` query parameter in url', () => {
+  const utmQueryParameter = 'utm_medium="test-content"';
+  const urlWithParam = `${LANDING_PAGE_URL_PATH}?${utmQueryParameter}`;
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Instant Observability/);
+  test('Header links to login', async ({ page, context }) => {
+    await page.goto(urlWithParam);
 
-  // create a locator
-  const searchInput = page.locator('input#catalogueSearch');
+    const loginLink = page.locator(
+      '//*[@id="gatsby-focus-wrapper"]/div[1]/div/ul/li/a',
+      { hasText: 'Log in' }
+    );
 
-  // Click the get started link.
-  await searchInput.fill('python');
-  await searchInput.press('Enter');
+    await loginLink.click();
+    await page.waitForTimeout(1000); // wait for the new tab to open
+    const pages = context.pages();
+    await expect(pages[1]).toHaveURL(/https:\/\/login\.newrelic.com\/login/);
+  });
 
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.+?search=python/);
+  test('New Relic logo is not clickable', async ({ page }) => {
+    await page.goto(urlWithParam);
+
+    const nrLogoLink = page.locator(
+      '//*[@id="gatsby-focus-wrapper"]/div[1]/div/a'
+    );
+
+    // Try clicking the logo
+    // `force` stops playwright from checking if the element
+    // can be clicked before attempting the click
+    await nrLogoLink.click({ force: true });
+    // The page should not have changed
+    await expect(page).toHaveURL(urlWithParam);
+  });
+
+  test('Platform, pricing, solutions, docs, and help center links are not visible', async ({
+    page,
+  }) => {
+    await page.goto(urlWithParam);
+
+    // Using Xpaths here because the location of these elements should not change
+    const platformLink = page.locator(
+      '//*[@id="gatsby-focus-wrapper"]/div[1]/div/nav/ul/li[1]/a',
+      { hasText: 'Platform' }
+    );
+    const pricingLink = page.locator(
+      '//*[@id="gatsby-focus-wrapper"]/div[1]/div/nav/ul/li[2]/a',
+      { hasText: 'Pricing' }
+    );
+    const solutionsLink = page.locator(
+      '//*[@id="gatsby-focus-wrapper"]/div[1]/div/nav/ul/li[3]/a',
+      { hasText: 'Solutions' }
+    );
+    const docsLink = page.locator(
+      '//*[@id="gatsby-focus-wrapper"]/div[1]/div/nav/ul/li[4]/a',
+      { hasText: 'Docs' }
+    );
+    const helpCenterLink = page.locator(
+      '//*[@id="gatsby-focus-wrapper"]/div[1]/div/nav/ul/li[5]/a',
+      { hasText: 'Help Center' }
+    );
+
+    expect(await platformLink.count()).toEqual(0);
+    expect(await pricingLink.count()).toEqual(0);
+    expect(await solutionsLink.count()).toEqual(0);
+    expect(await docsLink.count()).toEqual(0);
+    expect(await helpCenterLink.count()).toEqual(0);
+  });
 });
